9db960ddbfefe8833354643404e4bd8b3950b4e2
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-03-03 22:25:30 +0700

    Update Main.java

--- Assginment3/Main.java
+++ Assginment3/Main.java
@@ -3,8 +3,8 @@ package Assginment3;
 public class Main {
     public static void main(String[] args) {
         PersonModel<Student> student = new PersonModel<>();
-        student.addPerson(new Student("01","Huyen",19));
-        student.addPerson(new Student("02","Dat",17));
+        student.addPerson(new Student("01","AnhDaDen",19));
+        student.addPerson(new Student("02","AnhDaVang",17));
         student.Display();
         PersonModel<Employee> employee = new PersonModel<>();
         employee.addPerson(new Employee("01","Ngaos",690000));
